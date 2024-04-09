74d33f205442dfc64e5fe7901d5fef18738a4ce0
ahmadrosid <alahmadrosid@gmail.com>
2024-04-09 11:32:03 +0700

    chore: tidy up

--- parse.ts
+++ parse.ts
@@ -2,18 +2,22 @@ import fs from "fs"
 
 export const read = (path: string) => JSON.parse(fs.readFileSync(path).toString())
 
-// const key = "twemoji";
+// from iconsets: https://github.com/iconify/icon-sets
+
+// const key = "fluent-emoji";
 
 // const data = read(key + ".json")
-// const result = Object.keys(data.icons).filter(item => !item.startsWith("flag-for")).map((item) => ({
-//     url: `https://files.svgcdn.io/twemoji/${item}.svg`,
-//     key: item
-// }))
+// const result = Object.keys(data.icons)
+//     // .filter(item => !item.startsWith("flag-for"))
+//     .map((item) => ({
+//         url: `https://files.svgcdn.io/${key}/${item}.svg`,
+//         key: item
+//     }))
 
 // console.log(JSON.stringify(result))
 
-// const heroicons = read("lib/data/heroicons.json")
-// const twemoji = read("lib/data/twemoji.json")
-// const fxemoji = read("lib/data/fxemoji.json")
-// const emojis = twemoji.concat(fxemoji).concat(heroicons); 
-// console.log(JSON.stringify(emojis));
+const fluentemoji = read("fluentemoji.json")
+const emojis = read("lib/data/emojis.json")
+const res = fluentemoji.concat(emojis); 
+console.log(JSON.stringify(res));
+
